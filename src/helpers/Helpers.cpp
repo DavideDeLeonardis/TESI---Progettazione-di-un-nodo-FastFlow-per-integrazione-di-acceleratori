5e0931f9797d14a20a2596e112430c6b40627208
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2025-10-18 02:20:09 +0200

    rename from ns_ to _ns

--- src/helpers/Helpers.cpp
+++ src/helpers/Helpers.cpp
@@ -1,9 +1,11 @@
 #include "Helpers.hpp"
 #include <iostream>
 
-// Helper interno per estrarre il nome del file da un percorso, senza
-// estensione. Usata per estrapolare il nome della funzione kernel dal kernel
-// file.
+/**
+ * Helper interno per estrarre il nome del file da un percorso, senza
+ * estensione. Usata per estrapolare il nome della funzione kernel dal kernel
+ * file.
+ */
 static std::string extractKernelName(const std::string &path) {
    if (path.empty())
       return "";
@@ -20,7 +22,9 @@ static std::string extractKernelName(const std::string &path) {
    return filename.substr(0, dot_pos);
 }
 
-// Helper per il parsing degli argomenti della riga di comando.
+/**
+ * Helper per il parsing degli argomenti della riga di comando.
+ */
 void parse_args(int argc, char *argv[], size_t &N, size_t &NUM_TASKS,
                 std::string &device_type, std::string &kernel_path,
                 std::string &kernel_name) {
@@ -68,7 +72,9 @@ void parse_args(int argc, char *argv[], size_t &N, size_t &NUM_TASKS,
       kernel_name = extractKernelName(kernel_path);
 }
 
-// Helper per stampare la configurazione di esecuzione del programma.
+/**
+ * Helper per stampare la configurazione di esecuzione del programma.
+ */
 void print_configuration(size_t N, size_t NUM_TASKS,
                          const std::string &device_type,
                          const std::string &kernel_path) {
@@ -81,7 +87,9 @@ void print_configuration(size_t N, size_t NUM_TASKS,
    std::cout << "\n\n";
 }
 
-// Helper per stampare le istruzioni d'uso.
+/**
+ * Helper per stampare le istruzioni d'uso.
+ */
 void print_usage(const char *prog_name) {
    std::cerr << "Usage: " << prog_name
              << " [N] [NUM_TASKS] [DEVICE] [KERNEL_PATH]\n"
@@ -95,41 +103,49 @@ void print_usage(const char *prog_name) {
 }
 
 // Helper per stampare le statistiche finali.
-void print_stats(size_t N, size_t NUM_TASKS, const std::string &device_type,
-                 std::string &kernel_name, long long ns_elapsed,
-                 long long ns_computed, long long ns_total_InNode_time,
-                 long long ns_inter_completion_time, size_t final_count) {
+void calculate_and_print_metrics(size_t N, size_t NUM_TASKS,
+                                 const std::string &device_type,
+                                 std::string &kernel_name, long long elapsed_ns,
+                                 long long computed_ns,
+                                 long long total_InNode_time_ns,
+                                 long long inter_completion_time_ns,
+                                 size_t final_count) {
 
-   double elapsed_s = ns_elapsed / 1.0e9; // Trasformato in sec
-   double avg_InNode_time_ms =
-      (ns_total_InNode_time / final_count) / 1.0e6; // Trasformato in ms
-   double throughput = (elapsed_s > 0) ? (final_count / elapsed_s) : 0;
-   double avg_computed_ms =
-      (ns_computed / final_count) / 1.0e6; // Trasformato in ms
-   double avg_overhead_ms = avg_InNode_time_ms - avg_computed_ms;
-
+   // Tempo medio tra il completamento di due task consecutivi (in ms).
    double avg_service_time_ms = 0.0;
    if (final_count > 1)
       avg_service_time_ms =
-         (ns_inter_completion_time / (final_count - 1)) / 1.0e6;
+         (inter_completion_time_ns / (final_count - 1)) / 1.0e6;
+
+   // Tempo totale che la pipeline impiega per processare tutti i task (in sec).
+   double elapsed_s = elapsed_ns / 1.0e9;
+   // Tempo medio per un task dall'ingresso all'uscita del nodo (in ms).
+   double avg_InNode_time_ms = (total_InNode_time_ns / final_count) / 1.0e6;
+   // Tempo medio del singolo calcolo sull'acceleratore, senza overhead (in ms).
+   double avg_computed_ms = (computed_ns / final_count) / 1.0e6;
+   // Costo medio di gestione: trasferimento dati, uso delle code, etc.
+   double avg_overhead_ms = avg_InNode_time_ms - avg_computed_ms;
+   // Task totali processati al secondo.
+   double throughput = (elapsed_s > 0) ? (final_count / elapsed_s) : 0;
 
    std::cout << "\n------------------------------------------------------------"
                 "------\n"
-             << "PERFORMANCE METRICS on " << device_type << "\nN=" << N
+             << "PERFORMANCE METRICS on " << device_type << "\n   (N=" << N
              << ", Tasks=" << final_count;
 
    if (!kernel_name.empty())
       std::cout << ", Kernel=" << kernel_name;
 
    std::cout
-      << "\n------------------------------------------------------------------"
+      << ")\n------------------------------------------------------------------"
          "\n"
       << "Avg Service Time: " << avg_service_time_ms << " ms/task\n"
       << "   (Tempo medio tra il completamento di due task consecutivi)\n\n"
       << "Avg In_Node Time: " << avg_InNode_time_ms << " ms/task\n"
       << "   (Tempo medio per un task dall'ingresso all'uscita del nodo)\n\n"
       << "Avg Pure Compute Time: " << avg_computed_ms << " ms/task\n"
-      << "   (Tempo medio di calcolo sull'acceleratore, senza overhead)\n\n"
+      << "   (Tempo medio di un singolo calcolo sull'acceleratore, senza "
+         "overhead)\n\n"
       << "Avg Overhead Time: " << avg_overhead_ms << " ms/task\n"
       << "   (Costo medio di gestione: trasferimento dati, code, etc.)\n\n"
       << "Throughput: " << throughput << " tasks/sec\n"
--- src/helpers/Helpers.hpp
+++ src/helpers/Helpers.hpp
@@ -16,8 +16,11 @@ void parse_args(int argc, char *argv[], size_t &N, size_t &NUM_TASKS,
 // Helper per stampare le istruzioni d'uso.
 void print_usage(const char *prog_name);
 
-// Helper per stampare le statistiche finali.
-void print_stats(size_t N, size_t NUM_TASKS, const std::string &device_type,
-                 std::string &kernel_name, long long ns_elapsed,
-                 long long ns_computed, long long ns_total_InNode_time,
-                 long long ns_inter_completion_time, size_t final_count);
+// Helper per calcolare e stampare le metriche finali.
+void calculate_and_print_metrics(size_t N, size_t NUM_TASKS,
+                                 const std::string &device_type,
+                                 std::string &kernel_name, long long elapsed_ns,
+                                 long long computed_ns,
+                                 long long total_InNode_time_ns,
+                                 long long inter_completion_time_ns,
+                                 size_t final_count);
