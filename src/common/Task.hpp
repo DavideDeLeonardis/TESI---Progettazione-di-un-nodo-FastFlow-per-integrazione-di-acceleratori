626e77d37264ea28905c838da6c694d85e5b9279
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2025-10-18 14:26:29 +0200

    add handler for sync on metal

--- src/common/Task.hpp
+++ src/common/Task.hpp
@@ -13,10 +13,15 @@ struct Task {
    int *a, *b, *c; // Puntatori ai vettori di input/output
    size_t n;       // Dimensione dei vettori
 
-   size_t id{0};            // ID del task
-   size_t buffer_idx{0};    // Index del buffer set che il task sta usando
-   cl_event event{nullptr}; // Ultimo evento OpenCL generato
+   size_t id{0};         // ID del task
+   size_t buffer_idx{0}; // Index del buffer set che il task sta usando
 
-   // Tempo del task di ingresso nel nodo.
+   // Ultimo evento OpenCL generato (usato con GPU_openCL e FPGA).
+   cl_event event{nullptr};
+
+   // Handle generico per la sincronizzazione con GPU_Metal.
+   void *sync_handle{nullptr};
+
+   // Tempo di arrivo del task nel nodo.
    std::chrono::steady_clock::time_point arrival_time;
 };
