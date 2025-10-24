0cb6ace66d5220cd923a9c8901d5595d34b2d24c
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2025-10-24 15:00:18 +0200

    minor

--- src/helpers/Helpers.cpp
+++ src/helpers/Helpers.cpp
@@ -96,12 +96,12 @@ void print_configuration(size_t N, size_t NUM_TASKS, const std::string &device_t
  * Helper per stampare le istruzioni d'uso.
  */
 void print_usage(const char *prog_name) {
-   std::cerr << "Usage: " << prog_name << " [N] [NUM_TASKS] [DEVICE] [KERNEL_PATH]\n"
+   std::cerr << "Usage: " << prog_name << " [N] [NUM_TASKS] [DEVICE] [KERNEL]\n"
              << "  N            : Size of the vectors (default: 1,000,000)\n"
              << "  NUM_TASKS    : Number of tasks to run (default: 20)\n"
              << "  DEVICE       : 'cpu_ff', 'cpu_omp', 'gpu_opencl', 'gpu_metal' or 'fpga' "
                 "(default: 'cpu_ff').\n"
-             << "  KERNEL_PATH  : Path to the kernel file for accelerators (.cl, .xclbin, .metal)\n"
+             << "  KERNEL  : Path to the kernel file for accelerators (.cl, .xclbin, .metal)\n"
              << "                 or kernel name for CPU ('vecAdd', 'polynomial_op', etc.)\n"
              << "\nExample (GPU): " << prog_name
              << " 16777216 100 gpu_opencl kernels/gpu/heavy_compute_kernel.cl\n"
